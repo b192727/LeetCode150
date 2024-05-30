a14090b7dd57b0273bc79bd249a815914f71a0b4
Katroth Anil <158032470+ani2727@users.noreply.github.com>
2024-05-30 23:26:25 +0530

    Create BestTimetoBuyandSell.java

---
+++ BestTimetoBuyandSell.java
@@ -0,0 +1,23 @@
+//Problem Link: https://rb.gy/tnqcc0
+
+//code
+class Solution 
+{
+    public int maxProfit(int[] prices) 
+    {
+        int buyPrice = prices[0];
+        int profit = 0;
+
+        for (int i = 1; i < prices.length; i++) 
+        {
+            if (buyPrice > prices[i]) 
+            {
+                buyPrice = prices[i];
+            }
+
+            profit = Math.max(profit, prices[i] - buyPrice);
+        }
+
+        return profit;        
+    }
+}
